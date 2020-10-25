f9de67b939c87cc63456058d287cdcb1293b4887
abdulg95 <abdulg1995@gmail.com>
2020-10-25 18:55:24 -0400

    -get manga name and chapter from user input -create folders to save manga in

--- manga.py
+++ manga.py
@@ -1,23 +1,33 @@
 import re
 from bs4 import BeautifulSoup
 import requests
-import shutil
+import shutil,os
 img=[]
+
+name=raw_input("Enter manga name: ")
+chapter=raw_input("Enter chapter number: ")
+chapter=int(chapter)
+
 print("\nRunning...\n")
-for i in range(1,2):
-  for j in range(1,200):
-    URL = "http://www.mangapanda.com/onepunch-man/{}/{}".format(i,j)
-    request = requests.get(URL)
-    if request.status_code == 200:
-         page = requests.get(URL)
-         page_content = BeautifulSoup(page.content,'html.parser')
-         row_data=[]
-         for row in page_content.findAll('script',attrs={'type':"text/javascript"}):
-           row_data.append(row.text)
-         img.append(re.findall("[^.]ttps.*jpg",row_data[2]))
-    else:
-         break
+
+mangadestinationDir=name+' '+str(chapter)
+if not os.path.exists(mangadestinationDir): 
+				os.makedirs(mangadestinationDir)
+
+for j in range(1,200):
+  URL = "http://www.mangapanda.com/{}/{}/{}".format(name,chapter,j)
+  request = requests.get(URL)
+  if request.status_code == 200:
+        page = requests.get(URL)
+        page_content = BeautifulSoup(page.content,'html.parser')         
+        row_data=[]
+        for row in page_content.findAll('script',attrs={'type':"text/javascript"}):          
+          row_data.append(row.string)
+        img.append(re.findall("[^.]ttps.*jpg",row_data[2]))      
+  else:
+        break
 print("done loading chapters, now saving....")
+
 for i,x in enumerate(img):
   if x==[]:
     img.remove([])
@@ -25,7 +35,8 @@ for i in range(len(img)):
       Image_URL=''.join(map(str,img[i]))
       response = requests.get(Image_URL,stream=True)
       if response.status_code == 200:  
-          file = open("One_Punch_%d" % (i+1), 'wb')
+          file_name = "{}/{}{}".format(mangadestinationDir,name,i+1)
+          file = open(file_name, 'wb')
           response.raw.decode_content = True
           shutil.copyfileobj(response.raw,file)
           del response
